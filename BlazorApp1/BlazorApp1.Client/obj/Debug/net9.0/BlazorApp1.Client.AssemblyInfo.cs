d2d0dbda86a6633ffcf6b4df3aefd10ffe3e0f74
md3f4ltm <md3f4ltm@gmail.com>
2025-03-20 20:57:28 +0000

    update

--- BlazorApp1/BlazorApp1.Client/obj/Debug/net9.0/BlazorApp1.Client.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1.Client/obj/Debug/net9.0/BlazorApp1.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5377d19f6a7081250e0d1f2fce92f7ce7cc30a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+732480567329cf9c29e9ce1274c89bdda2efc033")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
